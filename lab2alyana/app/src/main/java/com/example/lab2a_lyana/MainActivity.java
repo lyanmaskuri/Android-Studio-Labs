14420c2fb8bc55164254ac1eb9587a84a8a70664
lyanmaskuri <maskurilyana0@gmail.com>
2026-09-05 21:52:34 +0800

    Add Lab2a

---
+++ lab2alyana/app/src/main/java/com/example/lab2a_lyana/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab2a_lyana;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button newpage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        newpage = (Button) findViewById(R.id.button);
+    }
+
+    public void openActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), NewActivity.class);
+        startActivity(intent);
+    }
+}
